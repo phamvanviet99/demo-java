661ad65f61e95c87c7e88746544727e3b14c6943
phamvanviet99 <52912843+phamvanviet99@users.noreply.github.com>
2025-10-23 14:13:42 +0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Value("${server.port}")
+	private String serverPort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("✅ Server started on port: " + serverPort);
+	}
 }
